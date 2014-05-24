b59f10e1a6fb6633a687f92c61cbcbe3c90cb1fb
Ivelin <ivelinkv@gmail.com>
2014-05-25 01:45:13 +0300

    Adding a stable version

--- src/Logic.java
+++ src/Logic.java
@@ -1,110 +1,82 @@
+import java.applet.Applet;
+import java.applet.AudioClip;
+import java.awt.BorderLayout;
 import java.awt.Graphics;
-import java.awt.Panel;
+import java.awt.image.BufferedImage;
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
 import java.net.URL;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
 
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
 import javax.swing.JLabel;
-import javax.swing.JPanel;
+import javax.swing.JOptionPane;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 
-
 public class Logic {
-
+	public static String[] images = { "01_base.png", "02_beamVertical.png",
+			"03_girderL.png", "04_girderR.png", "05_beamHorizontal.png",
+			"06_girderT.png", "07_SNARE.png", "008_head2.png", "009_Body2.png",
+			"010_handR2.png", "011_handL2.png", "012_FoodL2.png",
+			"012_FoodR2.png" };
 	static Graphics g;
-	static  String realWord;
+	static String realWord;
 	static char[] dots;
 	static StringBuffer guessWord;
-	static List<Character> usedChars = new ArrayList<Character>();
-	static String message;
-	static byte MAX_MISTAKES = 12;
+	static byte MAX_MISTAKES = 13;
 	static byte mistakes;
 	static boolean endIt = false;
-	
-	public static void main(String[] args) throws IOException {
-		
-		//First we initialize the game with InitGame()
-		//Then we call ProcessTurn(userInput) after a button is clicked
-//		Scanner in = new Scanner(System.in);
-//	InitGame();
-//	while (true) {
-//		String s = in.nextLine();
-//		ProcessTurn(s);
-//		paint(g);
-//		if (endIt)
-//			break;
-//	}
-//	
-	}
-	
-	//Start a new game
-	public static void InitGame() throws IOException { 
-			//Set errors to 0
+	static int minletters = 5;
+	static int maxletters = 13;
+
+	// Start a new game
+	public static void InitGame() {
+		// Set errors to 0
 		mistakes = 0;
-			//Set a new game (Forget about last win/lose)
+		// Set a new game (Forget about last win/lose)
 		endIt = false;
-	//Get a random word using a GET request to an API
-			 realWord = GetENWord().toLowerCase();
-			
-			//Make a masked with dots word that we can show to the user
-			dots = new char[realWord.length()];
-			for (int i = 0; i < dots.length; i++) {
-				dots[i] = '*'; 
-			}
-			//Show the user the masked word (We should edit a Label in the GUI)
-			System.out.println(dots);
-			
-			//Make a StringBuffer which we can later edit when we have a guess from the user (It firstly is the same as the mask word)
-			String s = new String(dots);
-			guessWord = new StringBuffer(s);
-			
-			message = "";
-			usedChars.clear();
+		// Get a random word using a GET request to an API
+		realWord = GetENWord().toLowerCase();
+
+		// Make a masked with dots word that we can show to the user
+		dots = new char[realWord.length()];
+		for (int i = 0; i < dots.length; i++) {
+			dots[i] = '*';
+		}
+		// Show the user the masked word (We should edit a Label in the GUI)
+		// System.out.println(dots);
+
+		// Make a StringBuffer which we can later edit when we have a guess from
+		// the user (It firstly is the same as the mask word)
+		String s = new String(dots);
+		guessWord = new StringBuffer(s);
+
+		try {
+			Paint(Home.guessWord);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
 	}
-	
-	//Call this method with userInput = TextBoxWithLetter.getText() 
-	private static void ProcessTurn(String userInput) {
-		
+
+	// Call this method with userInput
+	public static void ProcessTurn(String userInput) {
+		// Make the inputChar the input the user entered (The button he pressed)
 		char inputChar = userInput.toLowerCase().charAt(0);
-		
-		
-		//Check if the user input is a letter
-		if (!Character.isAlphabetic(inputChar)) {
-			message = "Input must be a letter";
-			return;
-		}
-		//Check if the user input is more than 1 letter
-		if (userInput.length() > 1) {
-			message = "Input must be 1 letter only";
-			return;
-		} 
-		//Check if user input isn't already used
-		if (usedChars.contains(inputChar)) {
-			message = "Letter already used";
-			return;
-		}
-		//Check if user hasn't already guessed this letter
-		if (new String(guessWord).indexOf(inputChar) != -1) {
-			message = "Letter has been already guessed";
-			return;
-		}
-		
-		
-		//If user didn't guess a letter
+
+		// If user didn't guess a letter
 		if (realWord.indexOf(inputChar) == -1) {
-			message = "";
 			mistakes++;
-			usedChars.add(inputChar);
 		}
-		//User guessed
+		// User guessed
 		if (realWord.indexOf(inputChar) != -1) {
 			for (int i = 0; i < realWord.length(); i++) {
 				if (realWord.charAt(i) == inputChar) {
@@ -112,118 +84,114 @@ public class Logic {
 				}
 			}
 		}
-		
-		//Check if max mistakes are reached
-				if (mistakes == MAX_MISTAKES) {
-					message = "You lost! The word you were searching for was: " + realWord + "\nClick \"Start New Game\"";
-					endIt = true;
-					return;
-				}
-				//Check if word is guessed
-				if (guessWord.indexOf("*") == -1) {
-					message = "You won! Click \"Start New Game\"";
-					endIt = true;
-					return;
-				}
-		
-	}
-	
-	
-	//Paint some stuff
-	  public static void paint(JLabel JguessWord) {
-			
-          //If you want more or less errors just change variable MAX_ERRORS and edit the below ifs (add or remove)
-
-          if (mistakes >  0){    
-        	//Show a picture
-        	  //g.DrawImage(pictureIllustrating 1 mistake)
-          }
-          if (mistakes ==  1){   
-        	//Show a picture
-          }
-          if (mistakes == 2){
-        	//Show a picture
-          }
-          if (mistakes == 3){
-        	//Show a picture
-          }
-          if (mistakes == 4){    
-        	//Show a picture
-          }
-          if (mistakes == 5){
-        	//Show a picture
-          }
-          if (mistakes == 6){    
-        	//Show a picture
-          }
-          if (mistakes == 7){   
-        	//Show a picture
-          }
-          if (mistakes == 8){
-        	//Show a picture
-          }
-          if (mistakes == 9){    
-        	//Show a picture
-          }
-          if (mistakes == 10){
-        	//Show a picture
-          }
-          if (mistakes == 11){    
-        	//Show a picture
-          }
-          if (mistakes == 12){
-        	//Show a picture
-          }
-          
-          // Draw the messages(Already used letter; More than one letter; etc..) + the masked word + all used chars
-          
-          JguessWord.setText(new String(guessWord));
-//			g.drawString( new String (guessWord), x, y);
-//			
-//          	for(Character usedchar : usedChars){
-//			g.drawString(usedchar + " ", x, y);
-//          	}
-          System.out.println(message);
-          System.out.println(guessWord);
-//    	for(Character usedchar : usedChars){
-//		System.out.println(usedchar + " ");
-//    	}
-    	System.out.println(usedChars + " ");
-    	System.out.println("Mistakes: " + mistakes);
-  
-  }
-	
-	
-		private static String GetENWord() {
-		      URL url;
-		      HttpURLConnection conn;
-		      BufferedReader rd;
-		      String line;
-		      String result = "";
-		      try {
-		         url = new URL("http://randomword.setgetgo.com/get.php");
-		         conn = (HttpURLConnection) url.openConnection();
-		         conn.setRequestMethod("GET");
-		         rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-		         while ((line = rd.readLine()) != null) {
-		            result += line;
-		         }
-		         rd.close();
-		      } catch (IOException e) {
-		         e.printStackTrace();
-		      } catch (Exception e) {
-		         e.printStackTrace();
-		      }
-		      return result;
-		   }
-		private static String GetBGWord() throws IOException {
-			Element word;
-			do {
-			Document doc = Jsoup.connect("http://rechnik.chitanka.info/random?").get();
-			 word = doc.getElementById("first-heading");
-		
-			} while (word.text().indexOf(" ") != -1);
-				return word.text();
+		// Paint the Hangman
+		try {
+			Paint(Home.guessWord);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
+		// Check if max mistakes are reached
+		if (mistakes == MAX_MISTAKES) {
+			URL url = null;
+			try {
+				url = new URL("file:lose.wav");
+			} catch (MalformedURLException e) {
+				e.printStackTrace();
+			}
+			// AudioClip clip = Applet.newAudioClip(url);
+			// clip.play();
 
+			try {
+				Thread.sleep(1);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+
+			JOptionPane.showMessageDialog(ENKeyboard.q,
+					"You lost! The word you were searching for was: "
+							+ realWord + "\nClick \"Start New Game\"",
+					"You Lose!", JOptionPane.PLAIN_MESSAGE);
+
+			endIt = true;
+		}
+		// Check if word is guessed
+		if (guessWord.indexOf("*") == -1) {
+			endIt = true;
+			URL url = null;
+			try {
+				url = new URL("file:win.wav");
+			} catch (MalformedURLException e) {
+				e.printStackTrace();
+			}
+			AudioClip clip = Applet.newAudioClip(url);
+			clip.play();
+
+			try {
+				Thread.sleep(1);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			JOptionPane.showMessageDialog(ENKeyboard.q, "YOU WIN!", "You WIN",
+					JOptionPane.PLAIN_MESSAGE);
+
+			endIt = true;
+		}
+	}
+
+	// Paint some stuff
+	public static void Paint(JLabel JguessWord) throws IOException {
+		BufferedImage myPicture = null;
+		if (mistakes != 0) {
+			myPicture = ImageIO.read(new File(images[mistakes - 1]));
+		} else {
+			myPicture = ImageIO.read(new File("blank.png"));
+		}
+		JLabel picLabel = new JLabel(new ImageIcon(myPicture));
+		UI.panel.add(picLabel, BorderLayout.EAST);
+
+		JguessWord.setText(new String(guessWord));
+		UI.panel.revalidate();
+	}
+
+	private static String GetENWord() {
+
+		URL url;
+		HttpURLConnection conn;
+		BufferedReader rd;
+		String line;
+		String result = "";
+		do {
+			result = "";
+			try {
+				url = new URL("http://randomword.setgetgo.com/get.php");
+				conn = (HttpURLConnection) url.openConnection();
+				conn.setRequestMethod("GET");
+				rd = new BufferedReader(new InputStreamReader(
+						conn.getInputStream()));
+				while ((line = rd.readLine()) != null) {
+					result += line;
+				}
+				rd.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			// TODO check why this code doesn't work as expected
+			// } while (result.length() <= minletters && result.length() >= maxletters
+		} while (result.length() >= maxletters);
+		return result;
+	}
+
+	private static String GetBGWord() throws IOException {
+		Element word;
+		do {
+			Document doc = Jsoup
+					.connect("http://rechnik.chitanka.info/random?").get();
+			word = doc.getElementById("first-heading");
+
+		} while (word.text().indexOf(" ") != -1
+				&& word.text().length() <= minletters
+				&& word.text().length() >= maxletters);
+		return word.text();
+	}
 }
